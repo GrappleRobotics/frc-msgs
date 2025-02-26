86238a7444d567193b6f0f8a379bcad7cd0e006b
Jaci Brunning <jaci.brunning@gmail.com>
2025-02-27 05:48:38 +1100

    Add UpdatePartV2 for field firmware upgrade

--- src/grapple/firmware.rs
+++ src/grapple/firmware.rs
@@ -3,7 +3,18 @@ use bounded_static::ToStatic;
 
 use crate::Validate;
 
-use super::{GrappleMessageId, errors::GrappleResult};
+use super::{errors::GrappleResult, GrappleMessageId, Request};
+
+// This will always fragment on CAN 2.0, but that's ok - it's faster and more resilient to dropped packets
+// since we can retry with a defined offset, so if the Ack gets lost that's also ok. 
+#[derive(Debug, Clone, PartialEq, Marshal, Demarshal, ToStatic)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))] 
+#[cfg_attr(feature = "schema", derive(schemars::JsonSchema))]
+#[repr(C)]
+pub struct UpdatePartV2Payload<'a> {
+  pub offset: u32,
+  pub payload: AsymmetricCow<'a, Payload>
+}
 
 #[derive(Debug, Clone, PartialEq, Marshal, Demarshal, MarshalUpdate, ToStatic)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize), serde(tag = "type", content = "data"))] 
@@ -22,7 +33,16 @@ pub enum GrappleFirmwareMessage<'a> {
   UpdatePartAck,
 
   #[marshal(tag = "3")]
-  UpdateDone
+  UpdateDone,
+
+  // This can be automatically detected by trying to perform UpdatePartV2, and if it fails,
+  // reverting back to UpdatePart (V1).
+  #[marshal(tag = "4")]
+  UpdatePartV2(
+    #[marshal(ctx = "forward")]
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    Request<UpdatePartV2Payload<'a>, GrappleResult<'a, ()>>
+  )
 }
 
 impl<'a> Validate for GrappleFirmwareMessage<'a> {
